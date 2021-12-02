97cc991f3a2728253247cb8cdc4d390d6b482052
Rafa leyva <rafaelleru95103@gmail.com>
2021-12-02 19:08:15 +0100

    use argument to select input file in main

--- 2/2.cpp
+++ 2/2.cpp
@@ -8,11 +8,11 @@ struct Indication {
 	std::string direction;
 };
 
-std::vector<Indication> parse_input(){
+std::vector<Indication> parse_input(std::string file_path){
 	int steps;
 	std::string direction;
 	Indication parsed_indication;
-std::ifstream infile("./input/input.txt");
+	std::ifstream infile(file_path);
 	std::vector<Indication> directions;
 
 	while (infile >> direction >> steps) {
@@ -63,14 +63,19 @@ std::pair<int, int> part_two(std::vector<Indication> input_directions){
 	return coordinates;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+	
+	if (argc != 2) {
+		std::cerr << "No input file provided" << std::endl;
+		exit(-1);
+	}
+	
+	std::string input_file = argv[1];
 
-	std::vector<Indication> input = parse_input();
+	std::vector<Indication> input = parse_input(input_file);
 	std::pair<int, int> sol1 = part_one(input);
 	std::pair<int, int> sol2 = part_two(input);
 	std::cout << "X: " << sol1.first << ", depth: " << sol1.second << ". Solution is: " << sol1.first*sol1.second << std::endl;
 	std::cout << "X: " << sol2.first << ", depth: " << sol2.second << ". Solution to part 2 is: " << sol2.first*sol2.second << std::endl;
 	return 0;
 }
-
-
