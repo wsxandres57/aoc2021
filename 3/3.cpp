7cc060c54157f74d92baec849a9efce3c9544bb2
Rafa leyva <rafaelleru95103@gmail.com>
2021-12-03 18:08:15 +0100

    Part 2 not working with the inputt file

--- 3/3.cpp
+++ 3/3.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bitset>
 #include <iostream>
 #include <fstream>
@@ -17,9 +18,8 @@ std::vector<std::string> parse_input(std::string input_file) {
 	return parsed_input;
 }
 
-int part_one(std::vector<std::string> input) {
+std::string get_ganma_rate(std::vector<std::string> input) {
 	std::string gannma_rate;
-	std::string epsilon_rate;
 
 	for (int i = 0; i < input[0].size(); ++i) {
 		int zeros = 0;
@@ -39,28 +39,80 @@ int part_one(std::vector<std::string> input) {
 		}
 	}
 
-	int gannma = stoi(gannma_rate, 0, 2);
-	/*  TODO: ~operator works but it prints -23, need to check how convert it to positive int
-	 *  int beta = ~ganma;
-	std::cout << gannma << std::endl;
-	std::cout << beta << std::endl;
-	 * */
+	return gannma_rate;
+}
 
-	for (char s: gannma_rate) {
-		if (s == '0') {
-			epsilon_rate.push_back('1');
-		} else {
-			epsilon_rate.push_back('0');
-		}
+std::string get_epsilon_rate(std::string ganma_rate) {
+	std::string epsilon_rate;
+
+	for (char s: ganma_rate) {
+               if (s == '0') {
+                       epsilon_rate.push_back('1');
+               } else {
+                       epsilon_rate.push_back('0');
+               }
+       }
+
+	return epsilon_rate;
+}
+
+std::string get_oxigen_rate(std::vector<std::string> input, std::string ganma_rate, bool epsilon) {
+	std::vector<std::string> aux = input;
+	std::vector<std::string> aux2;
+	int zeros = 0;
+	int ones = 0;
+	std::string oxigen_rate;
+
+
+	for (int i=0; i < ganma_rate.size()-1; ++i) {
+		std::copy_if(aux.begin(), aux.end(), std::back_inserter(aux2), 
+				[i, ganma_rate](std::string aux){ return aux[i] == ganma_rate[i]; });
+		aux = aux2;
+		std::cout << aux.size() << std::endl;
+		aux2.clear();
 	}
 
-	std::cout << epsilon_rate << std::endl;
-	int epsilon = std::stoi(epsilon_rate, 0, 2);
+	if (aux.size() == 2){ 
+		char s;
+		if (epsilon) {
+			s = '0';
+		} else {
+			s = '1';
+		}
 
-	std::cout << gannma << std::endl;
-	std::cout << epsilon << std::endl;
+		for (int i=0; i < aux.size(); ++i) {
+			if (aux[i][ganma_rate.size()-1] == s) {
+				oxigen_rate = aux[i];
+			}
+		}
+	} else {
+		std::cout << aux.size() << std::endl;
+		int zeros = 0;
+		int ones = 0;
+		for (int i=0; i < aux.size(); ++i) {
+			std::cout << aux[i] << std::endl;
+			if (aux[i][ganma_rate.size()-1] == '1') {
+				ones++;
+			} else {
+				zeros++;
+			}
+		}
+		
+		//std::cout << ones << std::endl;
+		//std::cout << zeros << std::endl;
 
-	return gannma*epsilon;
+		//char to_find = (ones>zeros) ? '0':'1';
+		//for (int i=0; i < aux.size(); ++i) {
+			//if (aux[i][ganma_rate.size()-1] == to_find) {
+				//oxigen_rate = aux[i];
+			//}
+		//}
+
+	}
+	
+	std::cout << "hola" << std::endl;
+
+	return oxigen_rate;
 }
 
 
@@ -74,8 +126,32 @@ int main(int argc, char* argv[]) {
 	std::string input_file = argv[1];
 
 	std::vector<std::string> input = parse_input(input_file);
-	int sol1 = part_one(input);
+	std::string ganma_rate = get_ganma_rate(input);
+	std::string epsilon_rate = get_epsilon_rate(ganma_rate);
+
+	int ganma = stoi(ganma_rate, 0, 2);
+	/*  TODO: ~operator works but it prints -23, need to check how convert it to positive int
+	 *  int beta = ~ganma;
+	std::cout << gannma << std::endl;
+	std::cout << beta << std::endl;
+	 * */
+	int epsilon = stoi(epsilon_rate, 0, 2);
+
+	int sol1 = ganma*epsilon;
 	std::cout << "Solution to part 1 is: " << sol1 << std::endl;
-	//std::pair<int, int> sol2 = part_two(input);
+
+	std::string oxigen_rate = get_oxigen_rate(input, ganma_rate, false);
+	std::string co2_rate = get_oxigen_rate(input, epsilon_rate, true);
+	std::cout << oxigen_rate << std::endl;
+	std::cout << co2_rate << std::endl;
+
+	//int oxigen = stoi(oxigen_rate, 0, 2);
+	//int co2 = stoi(co2_rate, 0, 2);
+	//int sol2 = oxigen*co2;
+	//std::cout << oxigen << std::endl;
+	//std::cout << co2 << std::endl;
+	//std::cout << "Solution to part 2 is: " << sol2 << std::endl;
+
+
 	return 0;
 }
