236da13419fbdd2600a2eb72c631bd7bbeb3e027
Rafa leyva <rafaelleru95103@gmail.com>
2021-12-06 20:41:38 +0100

    Solve Day 6 part 2

--- 6/6.cpp
+++ 6/6.cpp
@@ -1,10 +1,12 @@
+#include <algorithm>
+#include <numeric>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-void print_vector(std::vector<int>& v){
-	for(int& i: v)
+void print_vector(std::vector<u_int>& v){
+	for(u_int& i: v)
 		std::cout << i << ", ";
 
 	std::cout << std::endl;
@@ -42,10 +44,41 @@ int part_one(std::vector<int> intial_states){
 				intial_states[j] -= 1;
 			}
 		}
-		print_vector(intial_states);
 	}
 
 	return intial_states.size();
+}
+
+long long int part_two(std::vector<int> initial_states){
+	std::vector<long long int> fishes_by_state = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	for(int& i: initial_states)
+		fishes_by_state[i] += 1;
+
+	for (int i = 0; i < 256; ++i) {
+		long long int aux = fishes_by_state[8];
+		long long int aux2 = fishes_by_state[7];
+		long long int aux3 = fishes_by_state[6];
+		long long int aux4 = fishes_by_state[5];
+		long long int aux5 = fishes_by_state[4];
+		long long int aux6 = fishes_by_state[3];
+		long long int aux7 = fishes_by_state[2];
+		long long int aux8 = fishes_by_state[1];
+		long long int aux9 = fishes_by_state[0];
+
+		fishes_by_state[8] = aux9;
+		fishes_by_state[7] = aux;
+		fishes_by_state[6] = aux2 + aux9;
+		fishes_by_state[5] = aux3;
+		fishes_by_state[4] = aux4;
+		fishes_by_state[3] = aux5;
+		fishes_by_state[2] = aux6;
+		fishes_by_state[1] = aux7;
+		fishes_by_state[0] = aux8;
+	}
+
+	return std::accumulate(fishes_by_state.begin(), fishes_by_state.end(), 0LL);
+
 }
 
 int main(int argc, char* argv[]) {
@@ -56,9 +89,10 @@ int main(int argc, char* argv[]) {
 
 	std::string input_file = argv[1];
 	std::vector<int> initial_states = parse_input(input_file);
-	print_vector(initial_states);
 	int fishes = part_one(initial_states);
-	
 	std::cout << fishes << std::endl;
+
+	long long int fishes2 = part_two(initial_states);
+	std::cout << fishes2 << std::endl;
 
 }
