bb6820560b074cd8b69bfb7b853ee687e56db9c3
Rafa leyva <rafaelleru95103@gmail.com>
2021-12-10 22:43:40 +0100

    Solve Day 10 part 2

--- 10/10.cpp
+++ 10/10.cpp
@@ -11,7 +11,6 @@
 std::set<char> openings = {'(', '{', '<', '['};
 std::set<char> closings = {')', '}', '>', ']'};
 std::map<char, char> pairs = {{'(', ')'},{'{', '}'}, {'[', ']'}, {'<', '>'}};
-std::map<char, int> points = {{')', 3},{'}', 1197}, {']', 57}, {'>', 25137}};
 
 
 std::vector<std::string> parse_input(std::string input_file){
@@ -33,6 +32,7 @@ int get_line_punctuation(std::string line){
 	char corrupting_char;
 	int line_punctuation = 0;
 	std::stack<char> stack;
+	std::map<char, int> points = {{')', 3},{'}', 1197}, {']', 57}, {'>', 25137}};
 
 	for(const char& c: line){
 		// check if the character is a opening one
@@ -58,7 +58,6 @@ int get_line_punctuation(std::string line){
 }
 
 bool line_is_corrupt(std::string line){
-	
 	bool corrupt = false;
 	std::stack<char> stack;
 
@@ -90,7 +89,48 @@ int part_one(std::vector<std::string>& input){
 	int total_points = std::accumulate(puntuaciones.begin(), puntuaciones.end(), 0);
 
 	return total_points;
+}
 
+unsigned long long int complete_line(const std::string& line){
+	unsigned long long int punctuation = 0;
+	std::string closing_chars;
+	std::stack<char> stack;
+	std::map<char, int> points = {{')', 1},{'}', 3}, {']', 2}, {'>', 4}};
+
+	for(const char& c: line){
+		// check if the character is a opening one
+		bool opens = openings.find(c) != openings.end();
+		if (opens)
+			stack.emplace(c);
+		else {
+			char matching = stack.top();
+			stack.pop();
+		}
+	}
+
+	while (!stack.empty()) {
+		char c = stack.top();
+		stack.pop();
+		closing_chars.push_back(pairs[c]);
+
+		punctuation = punctuation*5 + points[pairs[c]];
+	}
+
+	return punctuation;
+}
+
+int part_two(std::vector<std::string>& input){
+	std::vector<unsigned long long int> puntuaciones;
+	for(const auto& line: input){
+		if(!line_is_corrupt(line)){
+			puntuaciones.push_back(complete_line(line));
+		}
+	}
+
+	auto m = puntuaciones.begin() + puntuaciones.size()/2;
+    std::nth_element(puntuaciones.begin(), m, puntuaciones.end());
+
+	return puntuaciones[puntuaciones.size()/2];
 }
 
 int main(int argc, char* argv[]) {
@@ -104,6 +144,8 @@ int main(int argc, char* argv[]) {
 	std::vector<std::string> input = parse_input(input_file);
 	int pt1 = part_one(input);
 	std::cout << pt1 << std::endl;
+	int pt2 = part_two(input);
+	std::cout << pt2 << std::endl;
 	
 	return 0;
 }
